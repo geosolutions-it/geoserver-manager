2e6a972f28e58e58d8d24d9cc0f0097185423872
carlo cancellieri <ccancellieri@hotmail.com>
2013-04-15 10:05:05 +0200

    clean GeoserverRESTDatastoreManagerTest license

--- src/test/java/it/geosolutions/geoserver/rest/manager/GeoserverRESTDatastoreManagerTest.java
+++ src/test/java/it/geosolutions/geoserver/rest/manager/GeoserverRESTDatastoreManagerTest.java
@@ -40,57 +40,6 @@ import org.junit.Test;
 /**
  * Test datastore handling (create, read and update):
  * 
- * <ul><li>Tests all the constructors and setters from
- * {@link GSDirectoryOfShapefilesDatastoreEncoder} and parent classes
- * ({@link GSShapefileDatastoreEncoder}, {@link GSAbstractDatastoreEncoder}).
- * 
- * <li>Tests constructors and getters from {@link RESTDataStore} (reader).
- * 
- * <li>Tests {@link GeoServerRESTDatastoreManager} create and update methods.</ul>
- * 
- * <p>The sequence is:
- * <ol>
- * <li>Create a DirectoryOfShapefilesDatastoreEncoder, with default parameters.
- * <li>Publish via GeoServerRESTDatastoreManager.create.
- * <li>Read the datastore from server.
- * <li>Test all parameter values.
- * <li>Create a new Encoder from it.
- * <li>Change all datastore parameter to non-default ones.
- * <li>Update via GeoServerRESTDatastoreManager.update.
- * <li>Read again.
- * <li>Test all new values.
- * </ol>
- * 
- * @author Oscar Fonts
- */
-/*
- *  GeoServer-Manager - Simple Manager Library for GeoServer
- *  
- *  Copyright (C) 2007,2012 GeoSolutions S.A.S.
- *  http://www.geo-solutions.it
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-/**
- * Test datastore handling (create, read and update):
- * 
  * <ul>
  * <li>Tests all the constructors and setters from {@link GSDirectoryOfShapefilesDatastoreEncoder} and parent classes (
  * {@link GSShapefileDatastoreEncoder}, {@link GSAbstractDatastoreEncoder}).
