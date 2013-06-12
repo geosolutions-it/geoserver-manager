f76434ea82aa655d15d1d60a4eea1fe0b2f60a2f
carlo cancellieri <ccancellieri@hotmail.com>
2013-06-12 11:36:42 +0200

    fix: missing jp2k support. close 77

--- src/main/java/it/geosolutions/geoserver/rest/encoder/coverage/GSImageMosaicEncoder.java
+++ src/main/java/it/geosolutions/geoserver/rest/encoder/coverage/GSImageMosaicEncoder.java
@@ -35,191 +35,267 @@ import org.jdom.Element;
 import org.jdom.filter.Filter;
 
 /**
+ * Use this encoder for ImageMosaic coverages.
  * 
  * @author Carlo Cancellieri - carlo.cancellieri@geo-solutions.it
- *
+ * 
  */
 public class GSImageMosaicEncoder extends GSCoverageEncoder {
 
-	final private static String STRING="string";
-	final private static String ENTRY="entry";
-	
-	final private NestedElementEncoder parameters=new NestedElementEncoder("parameters");
-	
-	public GSImageMosaicEncoder() {
+    final private static String STRING = "string";
+
+    final private static String ENTRY = "entry";
+
+    final private NestedElementEncoder parameters = new NestedElementEncoder("parameters");
+
+    public GSImageMosaicEncoder() {
         // Link members to the parent
-		addContent(parameters.getRoot());
-	}
-	
-	static class parametersFilter implements Filter {
-		final String name;
-		public parametersFilter(final String name){
-			this.name=name;
-		}
-		
-		private static final long serialVersionUID = 1L;
-		public boolean matches(Object obj) {
-			if (obj instanceof Element) {
-				if (((Element)obj).getName().equals(ENTRY)){
-					final Element el=((Element)obj).getChild(STRING);
-					if (el==null)
-						return false;
-					if (el.getText().equals(this.name)){
-						return true;
-					}
-				}
-			}
-			return false;
-		}
-	};
-	
-	private final boolean removeParameter(final Filter filter){
-		final Iterator<Element> it=ElementUtils.search(getRoot(), filter).iterator();
-		if (it.hasNext()){
-			final Element el=it.next();
-//			if (it.hasNext())
-//				return false;
-//			else
-			return ElementUtils.remove(el,el);
-		}
-		return false;
-	}
+        addContent(parameters.getRoot());
+    }
 
-	private final static String allowMultithreading="AllowMultithreading";
-	/**
-	 * @param val
-	 */
-	protected void addAllowMultithreading(final boolean val){
-		final List<Element> list=new ArrayList<Element>(2);
-		list.add(new Element(STRING).setText(allowMultithreading));
-		list.add(new Element(STRING).setText((val)?"true":"false"));
-		
-		parameters.add(null,list);
-	}
-	
-	private final static Filter allowMultithreadingFilter=new parametersFilter(allowMultithreading);
-	public void setAllowMultithreading(final boolean val){
-		removeParameter(allowMultithreadingFilter);
-		addAllowMultithreading(val);
-	}
-	
-	private final static String filter="Filter";
-	/**
-	 * @param val
-	 */
-	protected void addFilter(final String val){
-		final List<Element> list=new ArrayList<Element>(2);
-		list.add(new Element(STRING).setText(filter));
-		list.add(new Element(STRING).setText(val));
-		parameters.add(null,list);
-	}
-	
-	private final static Filter filterFilter=new parametersFilter(filter);
-	public void setFilter(final String val){
-		removeParameter(filterFilter);
-		addFilter(val);
-	}
+    static class parametersFilter implements Filter {
+        final String name;
 
-	private final static String maxAllowedTiles="MaxAllowedTiles";
-	/**
-	 * @param val
-	 */
-	protected void addMaxAllowedTiles(final int val){
-		final List<Element> list=new ArrayList<Element>(2);
-		list.add(new Element(STRING).setText(maxAllowedTiles));
-		list.add(new Element(STRING).setText(String.valueOf(val)));
-		parameters.add(null,list);
-	}
-	
-	private final static Filter maxAllowedTilesFilter=new parametersFilter(maxAllowedTiles);
-	public void setMaxAllowedTiles(final int val){
-		removeParameter(maxAllowedTilesFilter);
-		addMaxAllowedTiles(val);
-	}
-	
-	private final static String inputTransparentColor="InputTransparentColor";
-	/**
-	 * @param val
-	 */
-	protected void addInputTransparentColor(final String val){
-		final List<Element> list=new ArrayList<Element>(2);
-		list.add(new Element(STRING).setText(inputTransparentColor));
-		list.add(new Element(STRING).setText(val));
-		parameters.add(null,list);
-	}
-	
-	private final static Filter inputTransparentColorFilter=new parametersFilter(inputTransparentColor);
-	public void setInputTransparentColor(final String val){
-		removeParameter(inputTransparentColorFilter);
-		addInputTransparentColor(val);
-	}
-	
-	private final static String outputTransparentColor="OutputTransparentColor";
-	/**
-	 * @param val
-	 */
-	protected void addOutputTransparentColor(final String val){
-		final List<Element> list=new ArrayList<Element>(2);
-		list.add(new Element(STRING).setText(outputTransparentColor));
-		list.add(new Element(STRING).setText(val));
-		parameters.add(null,list);
-	}
-	
-	private final static Filter outputTransparentColorFilter=new parametersFilter(outputTransparentColor);
-	public void setOutputTransparentColor(final String val){
-		removeParameter(outputTransparentColorFilter);
-		addInputTransparentColor(val);
-	}
-	
-	private final static String SUGGESTED_TILE_SIZE="SUGGESTED_TILE_SIZE";
-	/**
-	 * @param val
-	 */
-	protected void addSUGGESTED_TILE_SIZE(final String val){
-		final List<Element> list=new ArrayList<Element>(2);
-		list.add(new Element(STRING).setText(SUGGESTED_TILE_SIZE));
-		list.add(new Element(STRING).setText(val));
-		parameters.add(null,list);
-	}
-	
-	private final static Filter SUGGESTED_TILE_SIZEFilter=new parametersFilter(SUGGESTED_TILE_SIZE);
-	public void setSUGGESTED_TILE_SIZE(final String val){
-		removeParameter(SUGGESTED_TILE_SIZEFilter);
-		addSUGGESTED_TILE_SIZE(val);
-	}
-	
-	private final static String USE_JAI_IMAGEREAD="USE_JAI_IMAGEREAD";
-	/**
-	 * @param val
-	 */
-	protected void addUSE_JAI_IMAGEREAD(final boolean val){
-		final List<Element> list=new ArrayList<Element>(2);
-		list.add(new Element(STRING).setText(USE_JAI_IMAGEREAD));
-		list.add(new Element(STRING).setText((val)?"true":"false"));
-		parameters.add(null,list);
-	}
-	
-	private final static Filter USE_JAI_IMAGEREADFilter=new parametersFilter(USE_JAI_IMAGEREAD);
-	public void setUSE_JAI_IMAGEREAD(final boolean val){
-		removeParameter(USE_JAI_IMAGEREADFilter);
-		addUSE_JAI_IMAGEREAD(val);
-	}
-	
-	private final static String backgroundValues="BackgroundValues";
-	/**
-	 * @param val
-	 */
-	protected void addBackgroundValues(final String val){
-		final List<Element> list=new ArrayList<Element>(2);
-		list.add(new Element(STRING).setText(backgroundValues));
-		list.add(new Element(STRING).setText(val));
-		parameters.add(null,list);
-	}
-	
-	private final static Filter backgroundValuesFilter=new parametersFilter(backgroundValues);
-	public void setBackgroundValues(final String val){
-		removeParameter(backgroundValuesFilter);
-		addBackgroundValues(val);
-	}
+        public parametersFilter(final String name) {
+            this.name = name;
+        }
+
+        private static final long serialVersionUID = 1L;
+
+        public boolean matches(Object obj) {
+            if (obj instanceof Element) {
+                if (((Element) obj).getName().equals(ENTRY)) {
+                    final Element el = ((Element) obj).getChild(STRING);
+                    if (el == null)
+                        return false;
+                    if (el.getText().equals(this.name)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    };
+
+    private final boolean removeParameter(final Filter filter) {
+        final Iterator<Element> it = ElementUtils.search(getRoot(), filter).iterator();
+        if (it.hasNext()) {
+            final Element el = it.next();
+            // if (it.hasNext())
+            // return false;
+            // else
+            return ElementUtils.remove(el, el);
+        }
+        return false;
+    }
+
+    private final Element getParameter(final Filter filter) {
+        final Iterator<Element> it = ElementUtils.search(getRoot(), filter).iterator();
+        if (it.hasNext()) {
+            final Element el = it.next();
+            return ElementUtils.contains(el, el);
+        }
+        return null;
+    }
+
+    private final static String allowMultithreading = "AllowMultithreading";
+
+    /**
+     * @param val
+     */
+    protected void addAllowMultithreading(final boolean val) {
+        final List<Element> list = new ArrayList<Element>(2);
+        list.add(new Element(STRING).setText(allowMultithreading));
+        list.add(new Element(STRING).setText((val) ? "true" : "false"));
+
+        parameters.add(null, list);
+    }
+
+    private final static Filter allowMultithreadingFilter = new parametersFilter(
+            allowMultithreading);
+
+    public void setAllowMultithreading(final boolean val) {
+        removeParameter(allowMultithreadingFilter);
+        addAllowMultithreading(val);
+    }
+
+    public Boolean getAllowMultithreading() {
+        Element el = getParameter(allowMultithreadingFilter);
+        if (el != null) {
+            List<Element> values = el.getChildren();
+            for (Element elm : values) {
+                String value = elm.getValue();
+                if (!value.equalsIgnoreCase(allowMultithreading)) {
+                    return Boolean.valueOf(value);
+                }
+            }
+        }
+        return null;
+    }
+
+    private final static String filter = "Filter";
+
+    /**
+     * @param val
+     */
+    protected void addFilter(final String val) {
+        final List<Element> list = new ArrayList<Element>(2);
+        list.add(new Element(STRING).setText(filter));
+        list.add(new Element(STRING).setText(val));
+        parameters.add(null, list);
+    }
+
+    private final static Filter filterFilter = new parametersFilter(filter);
+
+    public void setFilter(final String val) {
+        removeParameter(filterFilter);
+        addFilter(val);
+    }
+
+    private final static String maxAllowedTiles = "MaxAllowedTiles";
+
+    /**
+     * @param val
+     */
+    protected void addMaxAllowedTiles(final int val) {
+        final List<Element> list = new ArrayList<Element>(2);
+        list.add(new Element(STRING).setText(maxAllowedTiles));
+        list.add(new Element(STRING).setText(String.valueOf(val)));
+        parameters.add(null, list);
+    }
+
+    private final static Filter maxAllowedTilesFilter = new parametersFilter(maxAllowedTiles);
+
+    public void setMaxAllowedTiles(final int val) {
+        removeParameter(maxAllowedTilesFilter);
+        addMaxAllowedTiles(val);
+    }
+
+    private final static String inputTransparentColor = "InputTransparentColor";
+
+    /**
+     * @param val
+     */
+    protected void addInputTransparentColor(final String val) {
+        final List<Element> list = new ArrayList<Element>(2);
+        list.add(new Element(STRING).setText(inputTransparentColor));
+        list.add(new Element(STRING).setText(val));
+        parameters.add(null, list);
+    }
+
+    private final static Filter inputTransparentColorFilter = new parametersFilter(
+            inputTransparentColor);
+
+    public void setInputTransparentColor(final String val) {
+        removeParameter(inputTransparentColorFilter);
+        addInputTransparentColor(val);
+    }
+
+    private final static String outputTransparentColor = "OutputTransparentColor";
+
+    /**
+     * @param val
+     */
+    protected void addOutputTransparentColor(final String val) {
+        final List<Element> list = new ArrayList<Element>(2);
+        list.add(new Element(STRING).setText(outputTransparentColor));
+        list.add(new Element(STRING).setText(val));
+        parameters.add(null, list);
+    }
+
+    private final static Filter outputTransparentColorFilter = new parametersFilter(
+            outputTransparentColor);
+
+    public void setOutputTransparentColor(final String val) {
+        removeParameter(outputTransparentColorFilter);
+        addInputTransparentColor(val);
+    }
+
+    private final static String SUGGESTED_TILE_SIZE = "SUGGESTED_TILE_SIZE";
+
+    /**
+     * @param val
+     */
+    protected void addSUGGESTED_TILE_SIZE(final String val) {
+        final List<Element> list = new ArrayList<Element>(2);
+        list.add(new Element(STRING).setText(SUGGESTED_TILE_SIZE));
+        list.add(new Element(STRING).setText(val));
+        parameters.add(null, list);
+    }
+
+    private final static Filter SUGGESTED_TILE_SIZEFilter = new parametersFilter(
+            SUGGESTED_TILE_SIZE);
+
+    public void setSUGGESTED_TILE_SIZE(final String val) {
+        removeParameter(SUGGESTED_TILE_SIZEFilter);
+        addSUGGESTED_TILE_SIZE(val);
+    }
+
+    public String getSuggestedTileSize() {
+        Element el = getParameter(SUGGESTED_TILE_SIZEFilter);
+        if (el != null) {
+            List<Element> values = el.getChildren();
+            for (Element elm : values) {
+                String value = elm.getValue();
+                if (!value.equalsIgnoreCase(SUGGESTED_TILE_SIZE)) {
+                    return value;
+                }
+            }
+
+        }
+        return null;
+    }
+
+    private final static String USE_JAI_IMAGEREAD = "USE_JAI_IMAGEREAD";
+
+    /**
+     * @param val
+     */
+    protected void addUSE_JAI_IMAGEREAD(final boolean val) {
+        final List<Element> list = new ArrayList<Element>(2);
+        list.add(new Element(STRING).setText(USE_JAI_IMAGEREAD));
+        list.add(new Element(STRING).setText((val) ? "true" : "false"));
+        parameters.add(null, list);
+    }
+
+    private final static Filter USE_JAI_IMAGEREADFilter = new parametersFilter(USE_JAI_IMAGEREAD);
+
+    public void setUSE_JAI_IMAGEREAD(final boolean val) {
+        removeParameter(USE_JAI_IMAGEREADFilter);
+        addUSE_JAI_IMAGEREAD(val);
+    }
+
+    public Boolean getUseJaiImageRead() {
+        Element el = getParameter(USE_JAI_IMAGEREADFilter);
+        if (el != null) {
+            List<Element> values = el.getChildren();
+            for (Element elm : values) {
+                String value = elm.getValue();
+                if (!value.equalsIgnoreCase(USE_JAI_IMAGEREAD)) {
+                    return Boolean.valueOf(value);
+                }
+            }
+        }
+        return null;
+    }
+
+    private final static String backgroundValues = "BackgroundValues";
+
+    /**
+     * @param val
+     */
+    protected void addBackgroundValues(final String val) {
+        final List<Element> list = new ArrayList<Element>(2);
+        list.add(new Element(STRING).setText(backgroundValues));
+        list.add(new Element(STRING).setText(val));
+        parameters.add(null, list);
+    }
+
+    private final static Filter backgroundValuesFilter = new parametersFilter(backgroundValues);
+
+    public void setBackgroundValues(final String val) {
+        removeParameter(backgroundValuesFilter);
+        addBackgroundValues(val);
+    }
 
 }
